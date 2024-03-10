5dee4e40e528b38789d3c8d3edda4c13df9a4b50
worldwidepark <qkrguddn1066@naver.com>
2024-03-10 23:44:58 +0900

    update: current_user作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+  helper_method :current_user
+
+  private
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def authenticate_user
+    redirect_to login_url, alert: "Please log in." unless current_user
+  end
 end

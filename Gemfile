6fc057c28c652520398ac4f72b4bc7fc35e34d80
worldwidepark <qkrguddn1066@naver.com>
2024-04-06 00:11:35 +0900

    update: qrcode 追加

--- Gemfile
+++ Gemfile
@@ -44,6 +44,7 @@ gem 'cssbundling-rails'
 gem 'googleauth'
 gem 'dotenv-rails'
 gem "ruby-lsp"
+gem "rqrcode"
 # Use Active Storage variants [https://guides.rubyonrails.org/active_storage_overview.html#transforming-images]
 # gem "image_processing", "~> 1.2"
 
--- app/helpers/bango_huda_helper.rb
+++ app/helpers/bango_huda_helper.rb
@@ -1,2 +1,14 @@
+require "rqrcode"
 module BangoHudaHelper
+  def qrcode
+    # url入力
+    qrcode = RQRCode::QRCode.new("#{current_user.uuid}")
+    svg = qrcode.as_svg(
+      color: "000",
+      shape_rendering: "crispEdges",
+      module_size: 6,
+      standalone: true,
+      use_path: true
+    ).html_safe
+  end
 end
